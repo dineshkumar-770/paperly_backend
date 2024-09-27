88c99689dd66add8a99c290c363e5087a4ee8367
dineshkumar-770 <kumardinesh0699@gmail.com>
2024-09-27 10:11:08 +0530

    s3 prefix added in get_all_images

--- controller/all_wallpapers_cntroller.go
+++ controller/all_wallpapers_cntroller.go
@@ -19,23 +19,25 @@ import (
 )
 
 // RetrieveS3FileInstance
- 
-func RetrieveAllImageFromBucket(w http.ResponseWriter, r *http.Request){
- 
+
+func RetrieveAllImageFromBucket(w http.ResponseWriter, r *http.Request) {
+
 	w.Header().Set("Content-Type", "application/json")
 	resp := response.SuccessResponse{
 		Status: "Failed",
 	}
 	var allWallpapers []models.Wallpaper
 
-	_ = godotenv.Load(".env") 
+	_ = godotenv.Load(".env")
 	awsBucket := os.Getenv("BUCKETNAME")
+	folderS3 := os.Getenv("PRODUCTIONBUCKETFOLDER")
 	svc := helpers.GetAllFilesFromBucket()
 	input := &s3.ListObjectsV2Input{
-        Bucket: aws.String(awsBucket),
-    }
+		Bucket: aws.String(awsBucket),
+		Prefix: aws.String(folderS3 + "/"),
+	}
 	result, err := svc.ListObjectsV2(input)
-	if err !=nil{
+	if err != nil {
 		log.Println("Error in getting images from S3:-- ", err)
 		resp.Message = "Error in fetching Images from cloud"
 		resp.Data = err
@@ -44,21 +46,21 @@ func RetrieveAllImageFromBucket(w http.ResponseWriter, r *http.Request){
 		return
 	}
 	sort.Slice(result.Contents, func(i, j int) bool {
-        return result.Contents[i].LastModified.After(*result.Contents[j].LastModified)
-    })
+		return result.Contents[i].LastModified.After(*result.Contents[j].LastModified)
+	})
 
-	for _,item := range result.Contents{
+	for _, item := range result.Contents {
 		var wallpaper models.Wallpaper
-		if (strings.Contains(*item.Key,"category")){
-			fmt.Println("Contains Category Images : ", strings.Contains(*item.Key,"category"))
-		}else{
-			if isImage(*item.Key){ 
+		if strings.Contains(*item.Key, "category") {
+			fmt.Println("Contains Category Images : ", strings.Contains(*item.Key, "category"))
+		} else {
+			if isImage(*item.Key) {
 				req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
 					Bucket: aws.String(awsBucket),
 					Key:    aws.String(*item.Key),
 				})
-	
-				urlStr,err :=req.Presign(1 * time.Hour)
+
+				urlStr, err := req.Presign(1 * time.Hour)
 				if err != nil {
 					log.Println("error generating presigned URL for: --- ", err)
 					resp.Message = "Error in fetching Images from cloud"
@@ -67,33 +69,33 @@ func RetrieveAllImageFromBucket(w http.ResponseWriter, r *http.Request){
 					json.NewEncoder(w).Encode(resp)
 					break
 				}
-	
+
 				wallpaper.Size = *item.Size
 				wallpaper.Filename = urlStr
 				wallpaper.Category = ""
-	
+
 				allWallpapers = append(allWallpapers, wallpaper)
 			}
 		}
-		
+
 	}
 
-	if allWallpapers == nil{
+	if allWallpapers == nil {
 		w.WriteHeader(403)
 		resp.Message = "No images are available to display at the moment."
 		resp.Status = "Failed"
 		json.NewEncoder(w).Encode(resp)
 		return
-	} 
+	}
 	resp.Status = "Success"
 	resp.Message = "Successfully fetched all Images"
 	resp.Data = allWallpapers
 	w.WriteHeader(200)
-	json.NewEncoder(w).Encode(resp) 
- }
+	json.NewEncoder(w).Encode(resp)
+}
 
 func isImage(key string) bool {
-    lowerKey := strings.ToLower(key)
-    return strings.HasSuffix(lowerKey, ".jpg") || strings.HasSuffix(lowerKey, ".jpeg") ||
-        strings.HasSuffix(lowerKey, ".png") || strings.HasSuffix(lowerKey, ".gif")
+	lowerKey := strings.ToLower(key)
+	return strings.HasSuffix(lowerKey, ".jpg") || strings.HasSuffix(lowerKey, ".jpeg") ||
+		strings.HasSuffix(lowerKey, ".png") || strings.HasSuffix(lowerKey, ".gif")
 }
